18d6d802085724d1e048e050233e78e5dfacbf19
Boris <btadirov16@gmail.com>
2021-08-22 21:37:51 +0200

    asd

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var scores, roundScore, activePlayer, dice; 
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random()*6)+1;
+console.log(dice);
+
+// document.querySelector('#current--'+activePlayer).innerHTML='<em>'+dice+'</em>'
+document.querySelector('#current--'+activePlayer).textContent=dice;
